908863f4f688d088c2040a3b245ba1dae8a6d447
Matt Robinson <robinson.matty@gmail.com>
2015-01-13 00:05:23 -0500

    remove Initializer

--- lib/rcqrs.rb
+++ lib/rcqrs.rb
@@ -4,7 +4,6 @@ require 'active_job'
 require 'wisper'
 
 require 'rcqrs/support/guid'
-require 'rcqrs/support/initializer'
 
 require 'rcqrs/event_store/domain_event_storage'
 require 'rcqrs/event_store/domain_repository'
